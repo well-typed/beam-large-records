bcdb9a5b9f957224eb498fe5e73b489c7bd892c4
Edsko de Vries <edsko@well-typed.com>
2021-07-15 11:25:17 +0200

    Alternative approach
    This is a total rewrite of the library, starting with an observation
    from Andres that we could try to take advantage of the fact that we can
    use non-standard `Rep` instances for GHC generics, so that it looks as
    if `large-records` does in fact support GHC generics. This doesn't mean
    it's free: we still need to provide custom _instances_ for all the
    generic classes that `beam` uses, and there are quite a few of those.
    However, this provides a much smoother integration for the user of the
    library; there barely are differences now between using LR and not using
    LR, as far as beam integration is concerned. All that is required is
    
    ```haskell
    import Data.Record.Beam ()
    ```
    
    Indeed, apart from instances, there literally is nothing to import.
    
    This also enables us to reuse much more of beam internally, rather than
    having to duplicate a lot of its functionality.
    
    This rewrite offers all the functionality of the original library (the
    same test cases are still present), and significantly more: the three
    `beam` tutorials now all work with `large-records` (added test cases for
    these). Moreover, combinations of large-records and regular tables in
    a single database should now also be possible.

---
+++ test/Test/Record/Beam/SimpleSQL.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE KindSignatures        #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE UndecidableInstances  #-}
+
+{-# OPTIONS_GHC -F -pgmF=record-dot-preprocessor #-}
+
+-- | Simple but complete example that does an SQL INSERT and SELECT
+module Test.Record.Beam.SimpleSQL (tests) where
+
+import Data.Int
+import Data.Record.TH
+import Data.Text (Text)
+import Database.Beam
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import qualified Database.SQLite.Simple as SQLite
+import qualified GHC.Generics           as GHC
+
+import Data.Record.Beam ()
+
+import Test.Record.Beam.Util.SQLite
+
+{-------------------------------------------------------------------------------
+  Large record example
+-------------------------------------------------------------------------------}
+
+largeRecord defaultPureScript $ [d|
+      data LargeTable (f :: * -> *) = MkLargeTable {
+            largeTableId    :: Columnar f Int32
+          , largeTableField :: Columnar f Text
+          }
+        deriving (Show, Eq)
+    |]
+
+endOfBindingGroup
+
+large1, large2 :: LargeTable Identity
+large1 = [lr| MkLargeTable |] 1 "hi"
+large2 = [lr| MkLargeTable |] 2 "ho"
+
+instance Beamable LargeTable
+
+instance Table LargeTable where
+  newtype PrimaryKey LargeTable f = LargeTableKey (Columnar f Int32)
+    deriving stock (GHC.Generic)
+    deriving anyclass (Beamable)
+
+  primaryKey tbl = LargeTableKey tbl.largeTableId
+
+{-------------------------------------------------------------------------------
+  The full database
+-------------------------------------------------------------------------------}
+
+largeRecord defaultPureScript [d|
+    data ExampleDb (f :: * -> *) = MkExampleDb {
+          exampleDbLargeTable  :: f (TableEntity LargeTable)
+        }
+      deriving (Show)
+  |]
+
+instance Database be ExampleDb
+
+exampleDb :: DatabaseSettings be ExampleDb
+exampleDb = defaultDbSettings
+
+{-------------------------------------------------------------------------------
+  Tests proper
+-------------------------------------------------------------------------------}
+
+tests :: TestTree
+tests = testGroup "Test.Record.Beam.SimpleSQL" [
+      testCase "insert_select" test_insert_select
+    ]
+
+test_insert_select :: Assertion
+test_insert_select = runInMemory $ \conn -> do
+    liftIO $ SQLite.execute_ conn $
+      "CREATE TABLE db_large_table (table_id INT PRIMARY KEY NOT NULL, table_field VARCHAR NOT NULL);"
+
+    runInsert $
+      insert exampleDb.exampleDbLargeTable $ insertValues [
+          large1
+        , large2
+        ]
+
+    allLarge <- runSelectReturningList $ select $
+      orderBy_ (\x -> asc_ (x.largeTableId)) $ all_ exampleDb.exampleDbLargeTable
+    liftIO $ assertEqual "allLarge" allLarge [large1, large2]
+
